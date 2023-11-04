931f82d82bb9c826db65ab39c074c1f069c78c74
Jonatas Melo Silva <jonatas891024@gmail.com>
2023-11-04 11:42:33 -0300

    add: adiciona componente tipo use client

---
+++ src/components/form-search-products.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Search } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+export function FormSearchProducts() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const query = searchParams.get('q')
+
+  function handleSearch(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const query = formData.get('q') as string
+
+    if (!query) return null
+
+    router.push(`/search?q=${query}`)
+  }
+
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+    >
+      <Search className="h-5 w-5 text-zinc-500" />
+
+      <input
+        name="q"
+        type="text"
+        defaultValue={query ?? ''}
+        placeholder="Buscar produtos..."
+        className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+      />
+    </form>
+  )
+}
